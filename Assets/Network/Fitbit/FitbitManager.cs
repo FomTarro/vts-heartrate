0fd48a3eaf0e7721f1bc77a15bc7d050878d2bc4
Tom Farro <twfarro@wpi.edu>
2023-01-15 16:28:52 -0500

    add source for sdk4.3

--- Assets/Network/Fitbit/FitbitManager.cs
+++ Assets/Network/Fitbit/FitbitManager.cs
@@ -5,11 +5,13 @@ public class FitbitManager : Singleton<FitbitManager> {
 
 	private int _heartrate = 0;
 	public int Heartrate { get { return this._heartrate; } }
+	private string _appID = null;
+	private string _buildID = null;
 
 	[SerializeField]
 	private IServer _server = null;
 	private const int DEFAULT_PORT = 8215;
-	public int Port { get { return this._server != null ?  this._server.Port : DEFAULT_PORT; } }
+	public int Port { get { return this._server != null ? this._server.Port : DEFAULT_PORT; } }
 	public string LocalIP { get { return string.Format("{0}:{1}", HttpUtils.GetLocalIPAddress(), this._server.Port); } }
 
 	[SerializeField]
@@ -25,6 +27,7 @@ public class FitbitManager : Singleton<FitbitManager> {
 	private const int QR_PIXELS_PER_MODULE = 20;
 	private static QRCodeGenerator QR_GENERATOR = new QRCodeGenerator();
 
+	private const float TIMEOUT_MAX = 2f;
 	private float _timeout = -1f;
 
 	private System.Action<HttpUtils.ConnectionStatus> _onStatus = null;
@@ -36,9 +39,8 @@ public class FitbitManager : Singleton<FitbitManager> {
 
 	public void StartOnPort(int port, System.Action<HttpUtils.ConnectionStatus> onStatus) {
 		try {
-			this._timeout = 2f;
+			this._timeout = TIMEOUT_MAX;
 			port = HttpUtils.ValidatePortValue(port, DEFAULT_PORT);
-			Debug.Log("PORT " + port);
 			this._onStatus = onStatus;
 			this._server.SetPort(port);
 			this._server.StartServer();
@@ -48,15 +50,11 @@ public class FitbitManager : Singleton<FitbitManager> {
 			Debug.LogFormat("Fitbit Server started on port {0}.", port);
 		}
 		catch (System.Exception e) {
-			this._timeout = -1f;
-			HttpUtils.ConnectionStatus status = new HttpUtils.ConnectionStatus();
-			status.status = HttpUtils.ConnectionStatus.Status.ERROR;
-			status.message = e.Message;
-			this._onStatus.Invoke(status);
+			HandleException(e);
 		}
 	}
 
-	public void SetSelectedModel(FitbitModel model){
+	public void SetSelectedModel(FitbitModel model) {
 		this._selectedModel = model;
 	}
 
@@ -71,27 +69,51 @@ public class FitbitManager : Singleton<FitbitManager> {
 				break;
 		}
 		return EncodeString(
-			toEncode, 
-			Color.black, 
+			toEncode,
+			Color.black,
 			Color.white);
 	}
 
 	public void ReceiveData(string body) {
-		FitbitResult result = JsonUtility.FromJson<FitbitResult>(body);
-		this._heartrate = result.value;
-		this._timeout = 2f;
+		try {
+			FitbitResult result = JsonUtility.FromJson<FitbitResult>(body);
+			this._heartrate = result.value;
+			if (result.appID != null && result.appID != this._appID) {
+				Debug.Log(string.Format("Connection started with Fitbit device. (App ID: {0} / Build ID: {1})", result.appID, result.buildID));
+				this._appID = result.appID;
+				this._buildID = result.buildID;
+			}
+
+			this._timeout = TIMEOUT_MAX;
+			HttpUtils.ConnectionStatus status = new HttpUtils.ConnectionStatus();
+			status.status = HttpUtils.ConnectionStatus.Status.CONNECTED;
+			this._onStatus.Invoke(status);
+		}
+		catch (System.Exception e) {
+			HandleException(e);
+		}
+	}
+
+	private void HandleException(System.Exception e) {
+		this._timeout = -1f;
 		HttpUtils.ConnectionStatus status = new HttpUtils.ConnectionStatus();
-		status.status = HttpUtils.ConnectionStatus.Status.CONNECTED;
+		status.message = e.Message;
+		status.status = HttpUtils.ConnectionStatus.Status.ERROR;
 		this._onStatus.Invoke(status);
+		this._appID = null;
+		this._buildID = null;
 	}
 
 	private void Update() {
-		if(this._timeout > 0){
-		this._timeout = this._timeout - Time.deltaTime;
-			if(this._timeout <= 0){
+		if (this._timeout > 0) {
+			this._timeout = this._timeout - Time.deltaTime;
+			if (this._timeout <= 0) {
+				Debug.Log(string.Format("Fitbit device connection has been lost. (App ID: {0} / Build ID: {1})", this._appID, this._buildID));
 				HttpUtils.ConnectionStatus status = new HttpUtils.ConnectionStatus();
 				status.status = HttpUtils.ConnectionStatus.Status.DISCONNECTED;
 				this._onStatus.Invoke(status);
+				this._appID = null;
+				this._buildID = null;
 			}
 		}
 	}
@@ -106,14 +128,18 @@ public class FitbitManager : Singleton<FitbitManager> {
 	private static Texture2D EncodeString(string text, Color darkColor, Color lightColor) {
 		QRCodeGenerator.QRCode qrCode = QR_GENERATOR.CreateQrCode(text, QRCodeGenerator.ECCLevel.L);
 		Texture2D qrTexture = qrCode.GetGraphic(QR_PIXELS_PER_MODULE, darkColor, lightColor);
+		qrTexture.name = text;
 		return qrTexture;
 	}
 
 	[System.Serializable]
 	public class FitbitResult {
+		public string appID;
+		public string buildID;
 		public int value;
 	}
 
+	[System.Serializable]
 	public enum FitbitModel {
 		VERSA_3,
 		SENSE,
@@ -123,6 +149,7 @@ public class FitbitManager : Singleton<FitbitManager> {
 		VERSA
 	}
 
+	[System.Serializable]
 	public enum FitbitSDKVersion {
 		SDK_6_1,
 		SDK_4_3,
